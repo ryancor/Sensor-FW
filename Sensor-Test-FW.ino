1b0804a485fc1d0e0d5f000d923470592e1657f1
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-04-14 16:31:15 -0700

    adding distance ultra sonic sensor

--- Sensor-Test-FW.ino
+++ Sensor-Test-FW.ino
@@ -15,15 +15,24 @@ boolean takeLowTime;
  
 int pirPin = 3;    //the digital pin connected to the PIR sensor's output
 int ledPin = 13;
+
+const int TriggerPin = 7; // Trig pin
+const int EchoPin = 8; // Echo pin
+long Duration = 0;
  
  
 /////////////////////////////
 //SETUP
 void setup(){
   Serial.begin(9600);
+  
+  pinMode(TriggerPin,OUTPUT); // Trigger is an output pin
+  pinMode(EchoPin,INPUT); // Echo is an input pin
+  
   pinMode(pirPin, INPUT);
-  pinMode(ledPin, OUTPUT);
   digitalWrite(pirPin, LOW);
+  
+  pinMode(ledPin, OUTPUT);
  
   //give the sensor some time to calibrate
   Serial.print("calibrating sensor ");
@@ -40,7 +49,7 @@ void setup(){
 //LOOP
 void loop(){
      
-     if(digitalRead(pirPin) == HIGH){
+     if(digitalRead(pirPin) == HIGH) {
        digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
        if(lockLow){ 
          //makes sure we wait for a transition to LOW before any further output is made:
@@ -50,7 +59,16 @@ void loop(){
          digitalClockDisplay();
          //Serial.print(millis()/1000);
          //Serial.println(" sec ");
-         delay(50);
+
+         // Starting ping
+         Serial.println("Detecting Distance");
+         delay(2000);
+         for(int i = 0; i < 7; i++) {
+          loop_the_ping();
+         }
+         // End of ping
+        
+         delay(100);
          }        
          takeLowTime = true;
        }
@@ -97,3 +115,46 @@ void printDigits(int digits) {
     Serial.print('0');
   Serial.print(digits);
 }
+
+
+void loop_the_ping() {
+  digitalWrite(TriggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TriggerPin, HIGH); // Trigger pin to HIGH
+  delayMicroseconds(10); // 10us high
+  digitalWrite(TriggerPin, LOW); // Trigger pin to HIGH
+  
+  Duration = pulseIn(EchoPin, HIGH); // Waits for the echo pin to get high
+  // returns the Duration in microseconds
+  long Distance_mm = mm_distance(Duration); // Use function to calculate the distance
+  long Distance_in = in_distance(Duration); // Use function to calculate the distance
+  if(Distance_in < 500) {
+    Serial.print("Duration = ");
+    Serial.print((float)Duration/1000000);
+    Serial.print("   ");
+    Serial.print("MM = "); // Output to serial
+    Serial.print(Distance_mm);
+    Serial.print(" mm");
+    Serial.print("   ");
+    Serial.print("IN = ");
+    Serial.print(Distance_in);
+    Serial.println(" in");
+  } else {
+    Serial.println("Too Far.");
+  }
+  
+  delay(1000); // Wait to do next measurement
+}
+
+long mm_distance(long time) {
+  long DistanceCalc; // Calculation variable
+  DistanceCalc = ((time /2.9) / 2); // Actual calculation in mm
+  return DistanceCalc; // return calculated value
+}
+
+long in_distance(long time) {  
+  long DistanceCalc; // Calculation variable
+  DistanceCalc = time / 74 / 2; // Actual calculation in inches
+  return DistanceCalc; // return calculated value
+}
+
